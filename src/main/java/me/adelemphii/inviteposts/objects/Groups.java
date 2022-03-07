653e096fcb0dec69e5f826d6f46dee2dffaf0aa3
Adelemphii <demilitioowner@gmail.com>
2022-03-07 17:47:04 -0600

    webhook

--- src/main/java/me/adelemphii/inviteposts/objects/Groups.java
+++ src/main/java/me/adelemphii/inviteposts/objects/Groups.java
@@ -2,9 +2,9 @@ package me.adelemphii.inviteposts.objects;
 
 public enum Groups {
 
-    BUN_CHOPPA("Bun'choppa", "Adele", "https://discord.com/api/webhooks/950522516307337226/Tn6vUUpN_aOgY3637H7EcpE4BJZSP1psHXKRTeIak0C7dUooRnL4F9sk_5EjIOq6m99G"),
-    THE_POG_STOG("The POG STOG", "Pogchamp", "https://discord.com/api/webhooks/950522569109426207/3jAXwfgOc66t7MsWnP54b8zsnMtkothQIyLOgHLaoRDRRk5Me_ygqyAoor5I25WuEkiD"),
-    BUBBA("Bubba", "Bubba", "https://discord.com/api/webhooks/950522649107394580/ws7bhowkni5kwIl84NbGompsOpTH2e_SlqalSNidb99QodHUDbAkTl15AYT2pOBki6Zh");
+    BUN_CHOPPA("Bun'choppa", "Adele", "webhook"),
+    THE_POG_STOG("The POG STOG", "Pogchamp", "webhook"),
+    BUBBA("Bubba", "Bubba", "webhook");
 
     private String name;
     private String leader;
